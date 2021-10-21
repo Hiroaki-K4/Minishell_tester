57a28e868f108b613cb4aa61e6fb35a2e1e3b2e8
Hiroaki-K4 <49no19@gmail.com>
2021-10-21 16:05:30 +0900

    add run.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@
 # colors
 RESET="\033[0m"
 BOLDMAGENTA="\033[1m\033[35m"
-BOLDMAGENTA="\033[34m"
+BOLDGREEN="\033[1m\033[32m"
+
 
 cd ../minishell
 make re
@@ -10,7 +11,7 @@ cd ../Minishell_tester
 cp ../minishell/minishell .
 ./minishell
 
-printf "$BLUE __  __ _____ _   _ _____  _____ _    _ ______ _      _      \n"
+printf "$BOLDGREEN __  __ _____ _   _ _____  _____ _    _ ______ _      _      \n"
 printf "|  \/  |_   _| \ | |_   _|/ ____| |  | |  ____| |    | |     \n"
 printf "| \  / | | | |  \| | | | | (___ | |__| | |__  | |    | |     \n"
 printf "| |\/| | | | | . \` | | |  \___ \|  __  |  __| | |    | |     \n"
