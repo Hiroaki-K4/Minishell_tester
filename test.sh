88b41f01c352c251c043ebb81ca4f299dac10cd3
Hiroaki-K4 <49no19@gmail.com>
2021-10-21 16:04:14 +0900

    add run.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,13 @@
 # colors
 RESET="\033[0m"
 BOLDMAGENTA="\033[1m\033[35m"
+BLUE="\033[34m"
+
+cd ../minishell
+make re
+cd ../Minishell_tester
+cp ../minishell/minishell .
+./minishell
 
 printf "$BOLDMAGENTA __  __ _____ _   _ _____  _____ _    _ ______ _      _      \n"
 printf "|  \/  |_   _| \ | |_   _|/ ____| |  | |  ____| |    | |     \n"
@@ -9,9 +16,3 @@ printf "| \  / | | | |  \| | | | | (___ | |__| | |__  | |    | |     \n"
 printf "| |\/| | | | | . \` | | |  \___ \|  __  |  __| | |    | |     \n"
 printf "| |  | |_| |_| |\  |_| |_ ____) | |  | | |____| |____| |____ \n"
 printf "|_|  |_|_____|_| \_|_____|_____/|_|  |_|______|______|______|\n$RESET"
-
-cd ../minishell
-make re
-cd ../Minishell_tester
-cp ../minishell/minishell .
-./minishell
