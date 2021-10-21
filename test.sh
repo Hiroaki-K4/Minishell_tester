080aeb4d7059c344f15b5a9733ba46cd20aacf62
Hiroaki-K4 <49no19@gmail.com>
2021-10-21 16:16:24 +0900

    make run_test function

--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ cd ../minishell
 make re
 cd ../Minishell_tester
 cp ../minishell/minishell .
+chmod 755 minishell
 ./minishell
 
 printf "$BOLDGREEN __  __ _____ _   _ _____  _____ _ _     _ __          \n"
@@ -17,3 +18,11 @@ printf "| \  / | | | |  \| | | | | (___ | |__| | |__  | |    | |     \n"
 printf "| |\/| | | | | . \` | | |  \___ \|  __  |  __| | |    | |     \n"
 printf "| |  | |_| |_| |\  |_| |_ ____) | |  | | |____| |____| |____ \n"
 printf "|_|  |_|_____|_| \_|_____|_____/|_|  |_|______|______|______|\n$RESET"
+
+function run_test()
+{
+	TEST1=$(echo $@ "; exit" | ./minishell 2>&-)
+	echo $TEST1
+}
+
+run_test 'echo test'
